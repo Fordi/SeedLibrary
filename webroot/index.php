1761e4d5e422f8fc5fe3d3adb5bdc564e91c3491
Bryan Elliott <fordiman@gmail.com>
2011-06-05 22:30:37 -0400

    Added Tuffy font Modified htaccesses for wamp use broke up CSS Removed Gill Sans Built header shell Added comps

---
+++ webroot/index.php
@@ -0,0 +1,33 @@
+﻿<!DOCTYPE HTML>
+<html lang="en-US">
+	<head>
+		<meta charset="UTF-8">
+		<title>Jenkintown Seed Library (Composition)</title>
+		<link rel="stylesheet" href="css/reset.css" />
+		<link rel="stylesheet" href="font/tuffy.css" />
+		<link rel="stylesheet" href="css/page.css" />
+		<link rel="stylesheet" href="css/header.css" />
+	</head>
+	<body>
+		<div id="page">
+			<div id="page-header">
+				<h1>Jenkintown Library Community Seed Exchange</h1>
+				<div class="controls">
+					<ul>
+						<li><a href="/Login">Sign In</a></li>
+						<li><a href="/Contact">Contact Us</a></li>
+						<li><a href="/Donate">Donate</a></li>
+				</div>
+				<form class="search" action="/Search">
+					<label>Search</label>
+					<input type="text" class="text" name="q" />
+					<input type="submit" class="submit" value="&#x03D9;"/>
+				</div>
+			</div>
+			<div id="page-body">
+			</div>
+			<div id="page-footer">
+			</div>
+		</div>
+	</body>
+</html>
